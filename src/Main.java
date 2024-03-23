dcf45445d5e2e6efb4e88c8a064477b0bf73ba1a
emiiryv <111075935+emiiryv@users.noreply.github.com>
2024-03-23 12:28:34 +0300

    VYO

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("  __  __            _        _ ");
+        System.out.println(" |  \\/  | __ _ _ __| | _____| |_ ");
+        System.out.println(" | |\\/| |/ _` | '__| |/ / _ \\ __|");
+        System.out.println(" | |  | | (_| | |  |   <  __/ |_");
+        System.out.println(" |_|  |_|\\__,_|_|  |_|\\_\\___|\\__|");
+        System.out.println("                                 ");
         Panel panel = new Panel();
         panel.marketUrunleriOlustur();
         panel.musteriOlustur();
         panel.start();
+
+
     }
 }
